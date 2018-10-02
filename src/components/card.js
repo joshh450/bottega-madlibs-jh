d146ea3262e3a93018122b81a2fda9094c9aabdc
joshh450 <joshh450@yahoo.com>
2018-10-02 16:28:30 -0600

    built our first class component for the input container

---
+++ src/components/card.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Input from './input'
+
+class Card extends Component {
+    render() {
+        return (
+            <div className="card">
+                { Input('Cool') }
+                { Input('Plural noun') }
+            </div>
+        )
+    }
+}
+
+export default Card;
---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = (CustomLabel) => {
+    return (
+        <div className="input">
+            <input />
+            <label>{CustomLabel}</label>
+        </div>
+    )
+}
+
+export default Input;
